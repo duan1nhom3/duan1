97b0b67bb3d10121fca99282265017bdaac37732
linh <linhtxbph25719@fpt.edu.vn>
2022-11-26 09:52:08 +0700

    codelinh

---
+++ categories/add.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            text-align: center;
+        }
+        input{
+            width: 500px;
+            height: 30px;
+        }
+        button{
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Thêm mới hàng hóa</h1>
+    Mã loại <br>
+    <input type="text" disabled> </br>
+    Tên loại <br>
+     <input type="text"> <br>
+    <button>Thêm mới</button>
+</body>
+</html>
